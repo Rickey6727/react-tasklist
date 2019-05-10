477b0a38ad2ca877fcf20bb0ad4a7c716391b69a
中林 理力 <ririki-nakabayashi@united.jp>
2019-05-10 12:47:59 +0900

    hello world

---
+++ webpack.config.dev.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  mode: 'development',
+  entry:[
+    'webpack-hot-middleware/client',
+    path.resolve(__dirname, 'src/')
+  ],
+  output:{
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve:{
+    extensions: ['.js','.json','.jsx']
+  },
+  module:{
+    rules:[
+      {
+        test: /\.jsx?$/,
+        use:{
+          loader: 'babel-loader'
+        },
+        include: path.resolve(__dirname, 'src')
+      }
+    ]
+  }
+};
